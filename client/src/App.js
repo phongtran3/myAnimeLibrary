f13100b56a48d4edd269ef997729cf9d3cb7fb26
Phong <PhongTran3@my.unt.edu>
2023-02-08 23:26:53 -0600

    Added more dependencies to client side, initiated client architecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+const { BrowserRouter, Navigate, Routes, Route } = require("react-router-dom");
+const HomePage = require("../src/scenes/homePage");
+// const LoginPage = require("../src/scenes/login");
+const ProfilePage = require("../src/scenes/profilePage");
 
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="user/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

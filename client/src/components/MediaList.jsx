f27c1b97989690ad169daa23aa012d724e705dcf
Phong <PhongTran3@my.unt.edu>
2023-02-19 23:41:07 -0600

    Refactored media list to popper from popver

--- client/src/components/MediaList.jsx
+++ client/src/components/MediaList.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 //import {Card, Typography, CardMedia, Button, ButtonBase} from "@mui/material"
-import { ImageList, ImageListItem , ImageListItemBar, Typography } from '@mui/material';
+import { ImageList, ImageListItem , ImageListItemBar, Typography, Paper } from '@mui/material';
 import HoverPopover from "material-ui-popup-state/HoverPopover";
-import PopupState, {bindPopover, bindHover} from "material-ui-popup-state";
-
+import PopupState, {bindPopover, bindHover, bindToggle, bindPopper} from "material-ui-popup-state";
+import Popper from '@mui/material/Popper'
+import Fade from '@mui/material/Fade'
 
 export default function Media({media}) {
   //console.log(media);
+
   return (
     <ImageList cols={5} gap={48} sx={{textAlign: "center"}}>
-      {media.map(anime => ( 
-        <PopupState variant="popper" popupId="demo-popup-popover">
+      {media.map(anime => (
+        <PopupState variant="popper" popupId="demoPopper">
           {(popupState) => (
             <ImageListItem key={anime.id} {...bindHover(popupState)}>
             <a href={anime.siteUrl} target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none', color: 'inherit'}}>
@@ -22,20 +24,18 @@ export default function Media({media}) {
                   style={{borderRadius: "0.375rem", width: "230px", height: "326px"}}
               />
             <ImageListItemBar title={anime.title.english === null ? anime.title.romaji : anime.title.english}  position="below" sx={{maxWidth: "230px"}}/>
-              <HoverPopover
-              {...bindPopover(popupState)}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left"
-              }}
-            >
-              <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-            </HoverPopover>
             </a>
+            <Popper {...bindPopper(popupState)} transition placement="right-start">
+              {({ TransitionProps }) => (
+                <Fade {...TransitionProps}  >
+                  <Paper>
+                    <Typography >
+                      The content of the Popper.
+                    </Typography>
+                  </Paper>
+                </Fade>
+              )}
+            </Popper>
           </ImageListItem>
          )}
       </PopupState>

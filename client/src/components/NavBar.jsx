07daddf9458075c88e92ae11ca212f3b8681f33b
Phong <PhongTran3@my.unt.edu>
2023-03-06 23:44:06 -0600

    Added browse link with hovermenu

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,8 +4,27 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Link  } from 'react-router-dom';
 import {setMode, setLogout } from '../states/index';
-import { AppBar, Box, Typography, Toolbar, Avatar, Button, useScrollTrigger, Slide, useTheme, HideOnScroll, DarkMode, LightMode, } from '@mui/material';
+import { 
+  AppBar, 
+  Box, 
+  Typography, 
+  Toolbar, 
+  Avatar, 
+  Button, 
+  useScrollTrigger, 
+  Slide, 
+  useTheme, 
+  HideOnScroll, 
+  DarkMode, 
+  LightMode,
+  Menu,
+  MenuItem
+} from '@mui/material';
 
+
+import PopupState, {bindPopover, bindHover, bindToggle, bindPopper, bindMenu} from "material-ui-popup-state";
+import HoverMenu from 'material-ui-popup-state/HoverMenu'
+import HoverPopover from 'material-ui-popup-state/HoverPopover'
 export default function NavBar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -22,7 +41,9 @@ export default function NavBar() {
         <AppBar>
           <Toolbar>
             <Typography fontWeight="bold" fontSize="16px" component={Link} to="/">MyAnimeLibrary</Typography>
-            <Box display="inline-flex" justifyContent="center" alignItems="center" width="100%" sx={{"& > a": { padding:"0 12px"}}}> 
+            <Box display="inline-flex" justifyContent="center" alignItems="center" width="100%" 
+              sx={{"& > a": { padding:"0 12px", textDecoration:"none", color:"inherit"}}}
+            > 
               {user && (
                 <>
                 <Typography fontWeight="bold" fontSize="16px" component={Link} to={`/user/${userName}`} >Profile</Typography>
@@ -30,6 +51,17 @@ export default function NavBar() {
                 <Typography fontWeight="bold" fontSize="16px" component={Link} to={`/user/${userName}/mangalist`} >Manga List</Typography>
                 </>
               )}
+              <PopupState variant="popper" popupId="demoPopover">
+                  {(popupState) => ( 
+                    <>
+                      <Typography fontWeight="bold" fontSize="16px" {...bindHover(popupState)} component={Link} to="search/anime" >Browse</Typography>
+                      <HoverMenu {...bindMenu(popupState)}>
+                        <MenuItem onClick={popupState.close}><Typography fontWeight="bold" sx={{textDecoration:"none", color:"inherit"}} fontSize="16px" component={Link} to="search/anime">Anime</Typography></MenuItem>
+                        <MenuItem onClick={popupState.close}><Typography fontWeight="bold" sx={{textDecoration:"none", color:"inherit"}} fontSize="16px" component={Link} to="search/manga">Manga</Typography></MenuItem>
+                      </HoverMenu  >
+                    </>
+                  )}
+                </PopupState>
             </Box>
           </Toolbar>
         </AppBar>

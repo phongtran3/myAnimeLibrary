7752e049f5107aae67e655cbd68e3ea761791a91
Phong <PhongTran3@my.unt.edu>
2023-03-07 19:43:49 -0600

    Finished basic layout and functionalities of navbar, added profile icon button with dropmenu

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,6 +10,7 @@ import {
   Typography, 
   Toolbar, 
   Avatar, 
+  IconButton,
   Button, 
   useScrollTrigger, 
   Slide, 
@@ -20,7 +21,7 @@ import {
   Menu,
   MenuItem
 } from '@mui/material';
-import {PlayArrow, AutoStories} from "@mui/icons-material";
+import {PlayArrow, AutoStories, Logout, Settings, Person } from "@mui/icons-material";
 
 
 import PopupState, {bindPopover, bindHover, bindToggle, bindPopper, bindMenu} from "material-ui-popup-state";
@@ -35,16 +36,13 @@ export default function NavBar() {
   //const { userName } = useSelector((state) => state.user);
   const trigger = useScrollTrigger();
 
-
   return (
     <div>
       <Slide appear={false} direction="down" in={!trigger}>
         <AppBar>
           <Toolbar sx={{margin:"0 50px"}}>
             <Typography fontWeight="bold" fontSize="16px" component={Link} to="/">MyAnimeLibrary</Typography>
-            <Box display="inline-flex" justifyContent="center" alignItems="center" width="100%" 
-              sx={{"& > a": { padding:"0 12px", textDecoration:"none", color:"inherit"}, "button:last-child": {color:"red"}}}
-            > 
+            <Box display="inline-flex" justifyContent="center" alignItems="center" width="100%" sx={{"& > a": { padding:"0 12px", textDecoration:"none", color:"inherit"}}}> 
               {user && (
                 <>
                 <Typography fontWeight="bold" fontSize="16px" component={Link} to={`/user/${user.userName}`} >Profile</Typography>
@@ -52,19 +50,23 @@ export default function NavBar() {
                 <Typography fontWeight="bold" fontSize="16px" component={Link} to={`/user/${user.userName}/mangalist`} >Manga List</Typography>
                 </>
               )}
+              
               <PopupState variant="popper" popupId="demoPopover">
                   {(popupState) => ( 
                     <>
-                      <Typography fontWeight="bold" fontSize="16px" {...bindToggle(popupState)} component={Link} to="/search/anime" >Browse</Typography>
-                      <HoverMenu {...bindMenu(popupState)} sx={{width:"150px", padding:"20px"}}>
-                        <Box display="flex" alignItems="center">
-                          <Typography component={Link} to="/search/anime" display="flex" ><PlayArrow /></Typography >
-                          <MenuItem onClick={popupState.close}><Typography fontWeight="bold" sx={{textDecoration:"none", color:"inherit"}} fontSize="16px" component={Link} to="/search/anime">Anime</Typography></MenuItem>
+                      <Typography fontWeight="bold" fontSize="16px" {...bindHover(popupState)} component={Link} to="/search/anime" >Browse</Typography>
+                      <HoverMenu {...bindMenu(popupState)} sx={{"& .MuiPaper-root": {width:"175px", padding:"10px"}}}>
+                        <Box display="flex" alignItems="center" flexDirection="column">
+                          <Box display="flex" alignItems="center">
+                            <MenuItem><Typography component={Link} to="/search/anime" display="flex" ><PlayArrow /></Typography></MenuItem>
+                            <MenuItem ><Typography fontWeight="bold" sx={{textDecoration:"none", color:"inherit"}} fontSize="16px" component={Link} to="/search/anime">Anime</Typography></MenuItem>
+                          </Box>
+                          <Box display="flex" alignItems="center">
+                            <MenuItem><Typography component={Link} to="/search/manga" display="flex" ><AutoStories /></Typography></MenuItem>
+                            <MenuItem ><Typography fontWeight="bold" sx={{textDecoration:"none", color:"inherit"}} fontSize="16px" component={Link} to="/search/manga">Manga</Typography></MenuItem>
+                          </Box>
                         </Box>
-                        <Box>
-                          <MenuItem onClick={popupState.close}><Typography fontWeight="bold" sx={{textDecoration:"none", color:"inherit"}} fontSize="16px" component={Link} to="/search/manga">Manga</Typography></MenuItem>
-                        </Box>
-                      </HoverMenu  >
+                      </HoverMenu>
                     </>
                   )}
                 </PopupState>
@@ -72,11 +74,30 @@ export default function NavBar() {
                   <Typography fontWeight="bold" fontSize="16px" sx={{color:"white"}} component={Link} to="/auth">Login</Typography>
                 </Button> */}
             </Box>
-            <Box>
-               <Button variant="contained">
-                  <Typography fontWeight="bold" fontSize="16px" sx={{color:"white"}} component={Link} to="/auth">Login</Typography>
-                </Button> 
-            </Box>
+            {!user && (
+              <Box>
+                <Button variant="contained">
+                    <Typography fontWeight="bold" fontSize="16px" sx={{color:"white"}} component={Link} to="/auth">Login</Typography>
+                  </Button> 
+              </Box>
+              )}
+            {user && (
+              <PopupState variant="popper" popupId="demoPopover">
+              {(popupState) => ( 
+                <>
+                  <IconButton {...bindToggle(popupState)}><Avatar sx={{ width: 56, height: 56 }} src={`http://localhost:5000/assets/${user.picturePath}`}/></IconButton>
+                  <HoverMenu {...bindMenu(popupState)} sx={{"& .MuiPaper-root": {width:"175px", padding:"10px"}}}>
+                    <MenuItem onClick={() => {navigate(`/user/${user.userName}`)}}><Person/> Profile</MenuItem>
+                    <MenuItem><Settings/> Settings</MenuItem>
+                    <MenuItem onClick={() => dispatch(setLogout())}><Logout/> Logout</MenuItem>
+                  </HoverMenu>
+                </>
+              )}
+            </PopupState>
+
+            )}
+
+
           </Toolbar>
         </AppBar>
       </Slide>

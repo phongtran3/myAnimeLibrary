7ef3ae62abedc12b436f111e6ef2fb6487c7e26a
Phong <PhongTran3@my.unt.edu>
2023-04-10 22:10:49 -0500

    Refactored code to use array filter method

--- client/src/pages/listPage/ListPage.jsx
+++ client/src/pages/listPage/ListPage.jsx
@@ -4,12 +4,14 @@ import { useParams } from "react-router-dom";
 import { Box, Typography, useMediaQuery} from "@mui/material";
 import axios from 'axios';
 import NavBar from '../../components/NavBar'
+import ProfileCard from '../../components/ProfileCard'
 
 export default function ListPage() {
   const [user, setUser] = useState(null);
   const { userName, list } = useParams();
   const token = useSelector((state) => state.token);
   const type = list === 'animelist' ? 'anime' : 'manga'
+
   async function getUser(){
     await axios.get(
       `http://localhost:5000/users/${userName}`,
@@ -33,17 +35,22 @@ export default function ListPage() {
     return null;
   }
   const {firstName, lastName, animes, mangas, picturePath } = user.data;
-  const progress = [];
+  //const progress = [];
+  //const planning = []
   if (type === 'anime'){
-    for(let i = 0; i < animes.length; i++){
-      if(animes[i].userStatus === 'WATCHING')
-        progress.push(animes[i])
-    }
+      const progress = animes.filter(anime => anime.userStatus === 'WATCHING')
+      const completed = animes.filter(anime => anime.userStatus === 'COMPLETED')
+      const planning = animes.filter(anime => anime.userStatus === 'PLANNING')
+      console.log(progress);
+      console.log(planning);
+      console.log(completed);
   }else{
-    for(let i = 0; i < mangas.length; i++){
-      if(mangas[i].userStatus === 'READING')
-        progress.push(mangas[i])
-    }
+    const progress = mangas.filter(manga => manga.userStatus === 'READING')
+    const completed = mangas.filter(manga => manga.userStatus === 'COMPLETED')
+    const planning = mangas.filter(manga => manga.userStatus === 'PLANNING')
+    console.log(progress);
+    console.log(planning);
+    console.log(completed);
   }
 
   console.log(user)
@@ -52,8 +59,27 @@ export default function ListPage() {
     <>
       <NavBar />
       <Box id="content-container" margin="3em auto 0" maxWidth="1520px" padding="0 50px">
-        <h1>List Page</h1>
-
+        <Box id="content" 
+            display="grid"
+            gridTemplateColumns= "calc(40% - 30px) 60%"
+            gap="30px"
+        >
+           <Box id="section-1"> 
+            {/* will rename id later */}
+            <h1>List Page</h1>
+            <ProfileCard 
+              user={user} 
+              firstName={firstName} 
+              lastName={lastName} 
+              userName={userName} 
+              animes={animes} 
+              mangas={mangas} 
+              picturePath={picturePath} 
+              
+            />
+          </Box>
+           
+        </Box>
       </Box>
     </>
   )

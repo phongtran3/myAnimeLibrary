c6dfb7296459bc8211bcf8412e169a863e10ca9e
Phong <PhongTran3@my.unt.edu>
2023-08-21 19:04:38 -0500

    Removed local from key

--- server/controllers/s3Service.js
+++ server/controllers/s3Service.js
@@ -14,8 +14,8 @@ async function uploadToS3(req, res, next) {
   const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID_LOCAL,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_LOCAL,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
   });
 
